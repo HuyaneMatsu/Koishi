af857888eabdd08548e8536cde30b1c4d8b5fe46
HuyaneMatsu <re.ism.tm@gmail.com>
2021-04-11 20:09:27 +0200

    add nsfw role

--- bots/koishi.py
+++ bots/koishi.py
@@ -3,15 +3,15 @@ import re
 from datetime import datetime, timedelta
 
 from hata import BUILTIN_EMOJIS, sleep,  Client, KOKORO, cchunkify, alchemy_incendiary
-from hata.ext.commands import setup_ext_commands
+from hata.ext.command_utils import setup_ext_command_utils
 from hata.ext.slash import setup_ext_slash
 from hata.backend.futures import render_exc_to_list
 
 from bot_utils.tools import MessageDeleteWaitfor, GuildDeleteWaitfor, RoleDeleteWaitfor, ChannelDeleteWaitfor, \
     EmojiDeleteWaitfor, RoleEditWaitfor, ChannelCreateWaitfor, ChannelEditWaitfor
-from bot_utils.shared import PREFIX__KOISHI, category_name_rule, DEFAULT_CATEGORY_NAME, CHANNEL__NEKO_DUNGEON__SYSTEM, \
-    GUILD__NEKO_DUNGEON, ROLE__NEKO_DUNGEON__ANNOUNCEMENTS, ROLE__NEKO_DUNGEON__ELEVATED, COLOR__KOISHI_HELP, \
-    CHANNEL__SYSTEM__SYNC, ROLE__NEKO_DUNGEON__VERIFIED, CHANNEL__NEKO_DUNGEON__DEFAULT_TEST
+from bot_utils.shared import CHANNEL__NEKO_DUNGEON__SYSTEM, GUILD__NEKO_DUNGEON, ROLE__NEKO_DUNGEON__ANNOUNCEMENTS, \
+    ROLE__NEKO_DUNGEON__ELEVATED, CHANNEL__SYSTEM__SYNC, ROLE__NEKO_DUNGEON__VERIFIED, \
+    CHANNEL__NEKO_DUNGEON__DEFAULT_TEST, ROLE__NEKO_DUNGEON__NSFW_ACCESS
 
 
 from bot_utils.syncer import sync_request_waiter
@@ -22,7 +22,7 @@ _KOISHI_OMAE_RP = re.compile('omae wa mou', re.I)
 
 Koishi: Client
 
-setup_ext_commands(Koishi, lite=True)
+setup_ext_command_utils(Koishi)
 setup_ext_slash(Koishi)
 
 Koishi.events.message_create.append(CHANNEL__SYSTEM__SYNC, sync_request_waiter)
@@ -89,19 +89,24 @@ async def message_create(client, message):
 
 
 PATTERN_ROLE_RELATION = [
-    (re.compile('nya+', re.I), ROLE__NEKO_DUNGEON__VERIFIED, timedelta(minutes=10), True),
-    (re.compile('[il] *meow+', re.I), ROLE__NEKO_DUNGEON__ANNOUNCEMENTS, timedelta(), True),
-    (re.compile('[il] *not? *meow+', re.I), ROLE__NEKO_DUNGEON__ANNOUNCEMENTS, timedelta(), False),
-    (re.compile('nekogirl', re.I), ROLE__NEKO_DUNGEON__ELEVATED, timedelta(days=183), True),
+    (re.compile('nya+', re.I), ROLE__NEKO_DUNGEON__VERIFIED, timedelta(minutes=10), True, False),
+    (re.compile('[il] *meow+', re.I), ROLE__NEKO_DUNGEON__ANNOUNCEMENTS, None, True, False),
+    (re.compile('[il] *not? *meow+', re.I), ROLE__NEKO_DUNGEON__ANNOUNCEMENTS, None, False, False),
+    (re.compile('nekogirl', re.I), ROLE__NEKO_DUNGEON__ELEVATED, timedelta(days=183), True, True),
+    (re.compile('(?:i(?: *\'? *am|mma)?|me)? *horny *(?:desu)?', re.I), ROLE__NEKO_DUNGEON__NSFW_ACCESS, None, True, True),
+    (re.compile('no *sex', re.I), ROLE__NEKO_DUNGEON__NSFW_ACCESS, None, False, True),
         ]
 
 async def role_giver(client, message):
-    for pattern, role, delta, add in PATTERN_ROLE_RELATION:
+    for pattern, role, delta, add, requires_verify in PATTERN_ROLE_RELATION:
         
         if (pattern.fullmatch(message.content) is None):
             continue
         
         user = message.author
+        if requires_verify and (not user.has_role(ROLE__NEKO_DUNGEON__VERIFIED)):
+            break
+        
         if add == user.has_role(role):
             break
         
@@ -113,7 +118,7 @@ async def role_giver(client, message):
         if joined_at is None:
             break
         
-        if datetime.utcnow() - joined_at < delta:
+        if (delta is not None) and (datetime.utcnow() - joined_at < delta):
             break
         
         permissions = message.channel.cached_permissions_for(client)
