508575fb8c83f4abb715face0c9af59e86146400
HuyaneMatsu <re.ism.tm@gmail.com>
2022-06-04 11:36:19 +0200

    set query key if initializer is missing

--- bot_utils/model_linker.py
+++ bot_utils/model_linker.py
@@ -140,7 +140,7 @@ def get_new_method_string(fields, globals, added_initializer):
             code('self = INITIALIZER.__new__(cls, parent)')
         
         for field in fields:
-            if field.query_key is not None:
+            if (field.query_key is not None) and (added_initializer is not None):
                 continue
             
             if field.primary_key:
