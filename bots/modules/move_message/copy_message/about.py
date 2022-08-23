b77ba3349fefccf4f68d3d3ebaa508ca47f1e8a9
HuyaneMatsu <re.ism.tm@gmail.com>
2022-08-23 20:53:33 +0200

    resolve feedback of Koish

--- bots/modules/move_message/copy_message/about.py
+++ bots/modules/move_message/copy_message/about.py
@@ -1,7 +1,9 @@
 __all__ = ()
 
 from hata import BUILTIN_EMOJIS, Embed
+from hata.ext.slash import InteractionResponse
 
+from ..constants import ROLE__MEDIA_SORTER
 
 EXAMPLE_EMOJI = BUILTIN_EMOJIS['green_heart']
 
@@ -10,17 +12,25 @@ async def copy_message_about(client, event):
     """Shows the command's description."""
     name = client.name_at(event.guild_id)
     
-    return Embed(
-        'copy-message about',
-        (
-            f'`copy-message` is meant to help sort messages and posted media into their appropriate channels without '
-            f'deleting them it sends a copy of the message to a chosen channel by reacting with a (default) emoji of '
-            f'your choice, like {EXAMPLE_EMOJI} .\n'
-            f'\n'
-            f'The trigger emoji must be **uniquely** assigned to a **single** channel (name or topic), but the channel '
-            f'may contain multiple emojis.\n'
-            f'\n'
-            f'The user needs **manage messages** permission to invoke this command in both channels while {name} needs '
-            f'**manage webhooks** permission in the target channel.'
-        )
+    if event.guild_id == ROLE__MEDIA_SORTER.guild_id:
+        additional_info = f'or the {ROLE__MEDIA_SORTER:m} role'
+    else:
+        additional_info = ''
+    
+    return InteractionResponse(
+        embed = Embed(
+            'copy-message about',
+            (
+                f'`copy-message` is meant to help sort messages and posted media into their appropriate channels without '
+                f'deleting them. It sends a copy of the message to a chosen channel by reacting with a (default) emoji of '
+                f'your choice, like {EXAMPLE_EMOJI} .\n'
+                f'\n'
+                f'The trigger emoji must be **uniquely** assigned to a **single** channel (name or topic), but the channel '
+                f'may contain multiple emojis.\n'
+                f'\n'
+                f'The user needs **manage messages** permission {additional_info} to invoke this command in both '
+                f'channels while {name} needs **manage webhooks** permission in the target channel.'
+            )
+        ),
+        allowed_mentions = None,
     )
